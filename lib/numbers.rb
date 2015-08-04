c532d4c82833c8602aa823d4afbfbbf3f7731f0a
Guest User <student@epicodus.com>
2015-08-04 15:14:11 -0700

    Adds ability to translate two-digit numbers into written words.

--- lib/numbers.rb
+++ lib/numbers.rb
@@ -1,7 +1,18 @@
 class Fixnum
   define_method(:numbers) do
-    english_words = Hash.new()
-    english_words.store(1, "one")
-    english_words.fetch(1)
+    ones_digit = { 1 => "one"}
+    tens_digit = { 2 => "twenty"}
+
+    final_english = []
+    if self < 10
+      split_number_ones = self
+    else
+      split_number_tens = self./(10)
+      split_number_ones = self.%10
+      final_english.push(tens_digit.fetch(split_number_tens))
+    end
+
+    final_english.push(ones_digit.fetch(split_number_ones))
+    final_english.join(" ")
   end
 end
--- spec/numbers_spec.rb
+++ spec/numbers_spec.rb
@@ -5,4 +5,8 @@ describe('Fixnum#numbers') do
   it('Turns one digit into a written word') do
     expect((1).numbers()).to(eq("one"))
   end
+
+  it('Turns two digits into written words') do
+    expect((21).numbers()).to(eq("twenty one"))
+  end
 end
